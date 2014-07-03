fc920b35cd4faf5ba275af31cb79b4061edda678
csvenja <c.svenjax@gmail.com>
2014-07-03 01:54:02 -0700

    Calendar without collision. Use jQuery.

--- src/layOutDay.js
+++ src/layOutDay.js
@@ -1,13 +1,35 @@
+/* global Mustache:false */
+"use strict";
+
 function layOutDay(events) {
-	"use strict";
+	var eventList = $('#events');
+	$('#events').empty();
 	for (var i = 0; i < events.length; i++) {
-		currentEvent = new Event(events[i]);
-	};
+		var currentEvent = new Event(events[i]);
+		eventList.append(currentEvent.getHTML());
+	}
 }
 
 function Event(args) {
-	this.start = args[start];
-	this.end = args[end];
+	this.top = args.start;
+	this.height = args.end - args.start;
+	this.width = "100%";
 	this.title = "Sample Item";
 	this.location = "Sample Location";
-}
+};
+
+Event.prototype.getHTML = function() {
+	var template = $("#eventTemplate").html();
+	return Mustache.render(template, {
+		top: this.top,
+		height: this.height,
+		width: this.width,
+		title: this.title,
+		location: this.location
+	});
+};
+
+$(function() {
+	var events = [{start: 30, end: 150}];
+	layOutDay(events);
+})
